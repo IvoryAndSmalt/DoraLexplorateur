dc79e47fbaf1f6bafb8a251f42d5cb3ee79169ef
Lucas <lucas180395@gmail.com>
2018-11-15 17:04:22 +0100

    jeudi soir

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dora l'explorateur</title>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
@@ -17,19 +17,18 @@
 <div class="contenu">
 <?php
 $parent = "/var/www/html/";
-if(isset($_GET['dir'])){
+if (isset($_GET['dir'])) {
     liste($_GET['dir']);
-}
-else{
+} else {
     liste($parent);
 }
 
-function liste($dir){
-    if (isset($_GET['dir'])){
+function liste($dir)
+{
+    if (isset($_GET['dir'])) {
 
-    $dir = '/var/www/html/' . $_GET['dir'];
-    }
-    else {
+        $dir = '/var/www/html/' . $_GET['dir'];
+    } else {
         $dir = '/var/www/html/';
     }
     if ($handle = opendir($dir)) {
@@ -37,43 +36,43 @@ function liste($dir){
     </div>
 
 </header>
-    
+
 <!-- FONCTION AFFICHER LES DOSSIERS -->
 
 <div class="dossier">
     <?php
-    $fichiersAZipper = array();
+$fichiersAZipper = array();
         while (false !== ($en = readdir($handle))) {
-            if($en != '.' && $en != '..' && $en != '.git') {
-                
+            if ($en != '.' && $en != '..' && $en != '.git') {
+
                 array_push($fichiersAZipper, $en);
 
-                if(isset($_GET['dir'])){
-                    $lienget="?dir=".$_GET['dir'] . "/" . $en;
+                if (isset($_GET['dir'])) {
+                    $lienget = "?dir=" . $_GET['dir'] . "/" . $en;
                     $path_parts = pathinfo($_GET['dir'] . "/" . $en);
 
                     // S'IL Y A UNE EXTENSION
-                    if(isset($path_parts['extension'])){
+                    if (isset($path_parts['extension'])) {
                         $extension = $path_parts['extension'];
                         switch ($extension) {
                             case 'png';
                             case 'jpg';
                             case 'svg';
                             case 'gif';
-                            $sourceimg = "img/rose.png"
-                            ?>
+                                $sourceimg = "img/rose.png"
+                                ?>
                                 <div class="imalien">
-                                    <a href="/<?=$_GET['dir']."/".$en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
+                                    <a href="/<?=$_GET['dir'] . "/" . $en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
                                 </div>
                             <?php
                             break;
 
                             case 'php';
-                            $sourceimg = "img/pot3.png";
-                            ?>
+                                $sourceimg = "img/pot3.png";
+                                ?>
                                 <div class="imalien">
                                     <a href="/"></a>
-                                    <a href="/<?=$_GET['dir']."/".$en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
+                                    <a href="/<?=$_GET['dir'] . "/" . $en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
                                 </div>
                             <?php
                             break;
@@ -81,26 +80,26 @@ function liste($dir){
                             case 'html';
                             case 'css';
                             case 'js';
-                            $sourceimg = "img/pot1.png"
-                            ?>
+                                $sourceimg = "img/pot1.png"
+                                ?>
                                 <div class="imalien">
-                                    <a href="/<?=$_GET['dir']."/".$en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
+                                    <a href="/<?=$_GET['dir'] . "/" . $en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
                                 </div>
                             <?php
                             break;
 
                             default:
-                            $sourceimg = "img/citrouille.png"
-                            ?>
+                                $sourceimg = "img/citrouille.png"
+                                ?>
                                 <div class="imalien">
-                                    <a href="/<?=$_GET['dir']."/".$en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
+                                    <a href="/<?=$_GET['dir'] . "/" . $en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
                                 </div>
                             <?php
-                            break;  
+                            break;
                         }
                     }
                     // S'IL N'Y A PAS D'EXTENSION : C'EST UN DOSSIER
-                    else{
+                    else {
                         $sourceimg = "img/dossiers.png"
                         ?>
                         <div class="imalien">
@@ -110,7 +109,7 @@ function liste($dir){
                     }
                 }
                 // CAS OU ON EST A L'ACCUEIL
-                else if($en != 'index.html'){?>
+                else if ($en != 'index.html') {?>
                     <div class="imalien">
                         <a href="?dir=<?=$en?>"><img id="lien" src="img/dossiers.png" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
                     </div>
@@ -120,12 +119,14 @@ function liste($dir){
         }
     }
 }
-                ?>
+
+?>
 </div>
 
 <footer id="footer">
     <img src="img/tondeuse.png" alt="dessin tondeuse" id="toggleRight" class="voiture">
-    <img src="img/gazon.png" alt="gazon">
+    <div id="gazon">
+    </div>
 </footer>
 
 <script src="assets/js/script.js"></script>
