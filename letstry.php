aff4c65253e4d4a8ba6dc8e31a34d41118a455af
emilieq <emiliequinonero@gmail.com>
2018-11-16 14:03:50 +0100

    heureusement qu'emilie garde une copie .... vlan prends ta copie

--- letstry.php
+++ letstry.php
@@ -5,59 +5,146 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dora l'explorateur</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/stylemi.css">
 </head>
 
-<!-- Créer un  explorateur de fichiers en utilisant PHP. 
-L'explorateur doit permettre de naviguer dans tous 
-vos dossiers présents sur votre serveur local, 
-de visualiser les fichiers si le type le permet 
-(images, texte) et proposer le téléchargement sinon. 
-Vous devrez créer une identité visuelle ergonomique et 
-originale pour votre projet.
-Bonus : Intégrer votre projet avec Twig.  -->
-
 <body>
+<!-- HEADER ET CHEMIN -->
+<a id ="accueil" href="./letstry.php"><img src="img/accueil.png" alt="pancarte accueil"></a>
 
 <?php
-
-if (isset($_GET['dir'])){
+$parent = "../";
+if(isset($_GET['dir'])){
+    boutonRetour($_GET['dir']);
     liste($_GET['dir']);
+} else {
+    liste($parent);
+    boutonRetour($parent);
 }
 
-else{
-    liste('C:\wamp64\www\"');
+function boutonRetour($dir){
+    $retour = strrpos($dir, "/");
+    $retour = substr($dir, 0, $retour);
+    echo '<p><a id="retour" href="?dir='.$retour.'"> <img src="images/retour.png" alt="pancarte retour"> </a></p>';
 }
+function liste($dir)
+{
+    if (isset($_GET['dir'])) {
 
-function liste($dir){
-    if (isset($_GET['dir'])){
-        $parent = 'C:\wamp64\www\"';
-    $dir = 'C:\wamp64\www\"' . $_GET['dir'];
+    $dir = '../' . $_GET['dir'];
     }
     else {
-        $dir = 'C:\wamp64\www\"';
+        $dir = '../';
     }
-    if ($handle = opendir($dir)) {
-        echo "Vous êtes dans le dossier : $dir<br>";
-    
+    ?>
+    </div>
+
+<header id="header">
+    <?php
+if ($handle = opendir($dir)) {
+        echo "<p id='adresse'>" . str_replace("../", "Localhost", $dir) . "</p>"?>
+<!-- <h1>Dora L'explorateur</h1> -->
+
+
+
+
+</header>
+
+<!-- FONCTION AFFICHER LES DOSSIERS -->
+
+<div class="dossier">
+    <?php
+$fichiersAZipper = array();
         while (false !== ($en = readdir($handle))) {
-            if(isset($_GET['dir'])){ ?>
-                
-                <a href="letstry.php?dir=<?php echo $_GET['dir'] . "/" . $en?>"><?php echo $en?></a><br>
-                
-            <?php
-            }
-            else{
-                echo '<a href="letstry.php?dir='.$en . '">' . $en . '</a><br>';
+            if ($en != '.' && $en != '..' && $en != '.git') {
+
+                array_push($fichiersAZipper, $en);
+
+                if (isset($_GET['dir'])) {
+                    $lienget = "?dir=" . $_GET['dir'] . "/" . $en;
+                    $path_parts = pathinfo($_GET['dir'] . "/" . $en);
+
+                    // S'IL Y A UNE EXTENSION
+                    if (isset($path_parts['extension'])) {
+                        $extension = $path_parts['extension'];
+                        switch ($extension) {
+                            case 'png';
+                            case 'jpg';
+                            case 'svg';
+                            case 'gif';
+                                $sourceimg = "img/rose.png"
+                                ?>
+                                <div class="imalien">
+                                    <a href="/<?=$_GET['dir'] . "/" . $en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
+                                </div>
+                            <?php
+                            break;
+
+                            case 'php';
+                                $sourceimg = "img/pot3.png";
+                                ?>
+                                <div class="imalien">
+                                    <a href="/"></a>
+                                    <a href="/<?=$_GET['dir'] . "/" . $en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
+                                </div>
+                            <?php
+                            break;
+
+                            case 'html';
+                            case 'css';
+                            case 'js';
+                                $sourceimg = "img/pot1.png"
+                                ?>
+                                <div class="imalien">
+                                    <a href="/<?=$_GET['dir'] . "/" . $en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
+                                </div>
+                            <?php
+                            break;
+
+                            default:
+                                $sourceimg = "img/citrouille.png"
+                                ?>
+                                <div class="imalien">
+                                    <a href="/<?=$_GET['dir'] . "/" . $en?>" target="_blank"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
+                                </div>
+                            <?php
+                            break;
+                        }
+                    }
+                    // S'IL N'Y A PAS D'EXTENSION : C'EST UN DOSSIER
+                    else {
+                        $sourceimg = "img/dossiers.png"
+                        ?>
+                        <div class="imalien">
+                            <a href="<?=$lienget?>"><img id="lien" src="<?=$sourceimg?>" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
+                        </div>
+                    <?php
+                    }
+                }
+                // CAS OU ON EST A L'ACCUEIL
+                else if ($en != 'index.html') {?>
+                    <div class="imalien">
+                        <a href="?dir=<?=$en?>"><img id="lien" src="img/dossiers.png" alt="vlan le dossier"/><?="<p>" . $en . "</p>"?></a><br>
+                    </div>
+                <?php
+                }
             }
         }
     }
 }
-
 ?>
+</div>
+
+<footer id="footer">
+    <img src="img/tondeuse.png" alt="dessin tondeuse" id="toggleRight" class="voiture">
+    <div id="gazon">
+    </div>
+</footer>
 
 <script src="assets/js/script.js"></script>
 
 </body>
-
 </html>
+<!-- POUR SCANNER UN DOSSIER, UTILISER LE CHEMIN REEL
+POUR LIRE UN FICHIER EN PARTICULIER, UTILISER LE CHEMIN AVEC LOCALHOST, URL -->
